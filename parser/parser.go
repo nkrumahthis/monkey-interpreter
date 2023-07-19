1b20a206d28c8205181776d2da96414928e07dc9
Emmanuel Nkrumah-Sarpong <nkrumahthis@gmail.com>
2023-07-19 16:36:07 +0000

    ast

---
+++ parser/parser.go
@@ -0,0 +1,45 @@
+package parser
+
+import (
+	"github.com/nkrumahthis/monkey-interpreter/ast"
+	"github.com/nkrumahthis/monkey-interpreter/lexer"
+	"github.com/nkrumahthis/monkey-interpreter/token"
+)
+
+type Parser struct {
+	l *lexer.Lexer
+
+	curToken  token.Token
+	peekToken token.Token
+}
+
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{l: l}
+
+	// Read two tokens so that curToken and peekToken are both set
+	p.nextToken()
+	p.nextToken()
+
+	return p
+}
+
+func (p *Parser) nextToken() {
+	p.curToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+}
+
+func (p *Parser) ParseProgram() *ast.Program {
+	program := &ast.Program{}
+	program.Statements = []ast.Statement{}
+
+	for p.curToken.Type != token.EOF {
+		stmt := p.parseStatement()
+		if stmt != nil {
+			program.Statements = append(program.Statements, stmt)
+		}
+		p.nextToken()
+	}
+
+	return program
+}
+
