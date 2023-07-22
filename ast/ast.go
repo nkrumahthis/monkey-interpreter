435a616cced003a7c142a3655ba429dcb0877b08
Emmanuel Nkrumah-Sarpong <nkrumahthis@gmail.com>
2023-07-22 17:35:41 +0000

    parse return statements

--- ast/ast.go
+++ ast/ast.go
@@ -42,5 +42,13 @@ type Identifier struct {
 	Value string
 }
 
-func (i *Identifier) expressionNode()       {}
+func (i *Identifier) expressionNode()      {}
 func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
+
+type ReturnStatement struct {
+	Token       token.Token
+	ReturnValue Expression
+}
+
+func (rs *ReturnStatement) statementNode()       {}
+func (rs *ReturnStatement) TokenLiteral() string { return rs.Token.Literal }
